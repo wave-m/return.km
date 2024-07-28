6a0ddf39f28dce017d71706e1807d46858580b65
Kyeongmin Kim <wave-m@kakao.com>
2024-07-28 23:46:39 +0900

    feat: top-nav in header

--- shared/layout/header/header.ui.tsx
+++ shared/layout/header/header.ui.tsx
@@ -1,10 +1,12 @@
 import { ThemeMode } from './theme/theme.ui';
+import { TopNav } from './top-nav/top-nav.ui';
 
 export const Header = () => {
 	return (
 		<header className="sticky top-0 z-50 w-full">
 			<div className="container flex items-center">
 				{/* TODO: 좌측 메뉴 */}
+				<TopNav />
 				<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
 					<ThemeMode />
 				</div>
